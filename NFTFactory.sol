23c98a9a6104ff211c4b06cbadd408e2a4a556fb
puls369ar <155809127+puls369ar@users.noreply.github.com>
2024-12-05 15:50:07 +0400

    Update NFTFactory.sol

--- NFTFactory.sol
+++ NFTFactory.sol
@@ -6,7 +6,10 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "./SuperwineNFT.sol";
 
 contract NFTFactory is Ownable {
-    constructor() Ownable(msg.sender) {}
+    address feeCollector;
+    constructor(address _feeCollector) Ownable(msg.sender) {
+        feeCollector = _feeCollector;
+    }
     
     struct Collection {
         address contractAddress;
@@ -18,10 +21,11 @@ contract NFTFactory is Ownable {
         uint256 royalty;
     }
 
-    Collection[] public collections;
+    mapping(uint256 => Collection) public collections;
+    uint256 public collectionCount;
     mapping(address => bool) public isCollection;
 
-    event CollectionCreated(address indexed creator, address contractAddress, string name);
+    event CollectionCreated(address indexed creator, address contractAddress, string name, uint256 collectionId);
 
     function createCollection(
         string memory _name,
@@ -31,11 +35,11 @@ contract NFTFactory is Ownable {
         string memory _creator,
         string[] memory _tags,
         string memory _baseURI,
-        uint256 _supply,
-        uint256 _royalty
-    ) external onlyOwner {
-        SuperwineNFT collection = new SuperwineNFT(_name, _symbol, _baseURI);
-        collections.push(Collection({
+        uint96 _royalty
+    ) external onlyOwner returns(uint256) {
+        
+        SuperwineNFT collection = new SuperwineNFT(_name, _symbol, _baseURI, feeCollector, _royalty);
+        collections[collectionCount++] = Collection({
             contractAddress: address(collection),
             name: _name,
             description: _description,
@@ -43,18 +47,23 @@ contract NFTFactory is Ownable {
             creator: _creator,
             tags: _tags,
             royalty: _royalty
-        }));
+        });
 
         isCollection[address(collection)] = true;
 
-        emit CollectionCreated(msg.sender, address(collection), _name);
+        emit CollectionCreated(msg.sender, address(collection), _name, collectionCount);
 
+
+        return collectionCount;
+
+    }
+
+    function mintNFTs(uint256 collectionId, uint256 _supply) external {
+        SuperwineNFT collection = SuperwineNFT(collections[collectionId].contractAddress);
         collection.mint(msg.sender, _supply);
     }
 
-    function getCollections() external view returns (Collection[] memory) {
-        return collections;
-    }
+    
 }
 
 
